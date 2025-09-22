1529b392fed2169fc2607cf115f410add982bbbb
Liam Von Astoria <hello@liamvonastoria.net>
2025-09-22 18:01:09 +0200

    init commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,12 +12,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-img-element": "off",
-      "jsx-a11y/alt-text": "off",
-    },
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
   },
 ];
 
